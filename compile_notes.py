ff78b514b004e04cbc9d6ccef70f19e87e2ae3eb
Francesco Evangelista <francesco.evangelista@gmail.com>
2020-09-14 14:51:13 -0400

    Add pdf of Ch 5

--- compile_notes.py
+++ compile_notes.py
@@ -6,17 +6,18 @@ from shutil import copy
 chapters = ['01-Basics',
             '02-BornOppenheimer',
             '03-Hartree-Fock',
-            '04-StationaryPoints']
+            '04-StationaryPoints',
+            '05-GeometryOptimization']
 
 root_dir = os.getcwd()
 
 for ch in chapters:
     print(ch)
-    chapter_dir = os.path.join('src',ch)
+    chapter_dir = os.path.join('Notes',ch)
     os.chdir(chapter_dir)
     os.system(f'xelatex {ch}.tex')
     os.system(f'bibtex {ch}.tex')
     os.system(f'xelatex {ch}.tex')
     os.system(f'xelatex {ch}.tex')
-    copy(f'{ch}.pdf', f'../pdfs')
+    copy(f'{ch}.pdf', f'../../pdfs')
     os.chdir(root_dir)
